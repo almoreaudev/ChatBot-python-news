c17034fad8e77b0cc0831f714c03646a7ab2a730
Alexandre Moreau <almoreaudev@gmail.com>
2024-06-18 14:23:31 +0200

    Feat : add fonction load_json_bodies_in_Document_list
    Ajout de la fonction load_json_bodies_in_Document_list.
    Document est une classe de langchain, exploitable pour la suite du projet.
    
    Suppression des fonctions "preprocess_articles", inutile pour la suite.

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -1,26 +1,27 @@
 #importe les régulars expression pour supprimer les balises HTML
 import re
-from src.data_collection import get_guardian_articles_data
+from src.data_collection import *
 
-# Récupère un "article", une multi-liste comportant les informations d'un article 
-# rend un dictionnaire avec deux keys 'headline' et 'body'
-def preprocess_article(article):
-    headline = article['fields']['headline']
-    body = article['fields']['body']
-    body = re.sub('<[^<]+?>', '', body)
-    return {'headline': headline, 'body': body}
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import JSONLoader
+from langchain_core.documents import Document
+
+from bs4 import BeautifulSoup
 
 '''
-Fonction preprocess_articles(...)
+Fonction load_json_body()
 
-Argument :
-query --> sur quoi se base la recherche pour l'API (rien "", "technology", "football", etc)
+Charge le fichier JSON gràce à JSONLoader, un module pour charger les documents JSON pour l'utilisation de langchain
+Charge uniquement les balises "body" des résultats du fichier JSON (pas toute l'arborescence)
 
-Retour : 
-Une liste de dictionnaire ayant deux keys pour chaque élément "headline" et "body"
+Retour :
+Une liste de "Document", un format spécial de langchain pour ajouter les données à ChromaDB
 '''
-def preprocess_articles(query=""):
-    data = get_guardian_articles_data(query, pagesize=1)
-    articles = data['response']['results']       
-    
-    return [preprocess_article(article) for article in articles]
+def load_json_bodies_in_Document_list() -> list[Document]:
+    loader = JSONLoader(
+    file_path='data/articles.json',
+    jq_schema='.response.results[].fields.body')
+
+    documents = loader.load()
+    return documents
+
