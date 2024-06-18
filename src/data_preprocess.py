16087e3abf99a204617ae604009065fb3834cd91
Alexandre Moreau <almoreaudev@gmail.com>
2024-06-18 15:13:35 +0200

    Feat : add function split_data
    La fonction split_data prends une liste de Document et la transforme en une liste de document ayant été splitté.
    Elle enlève aussi les balise HTML des données gràce à BeautifulSoup.

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -20,8 +20,29 @@ Une liste de "Document", un format spécial de langchain pour ajouter les donné
 def load_json_bodies_in_Document_list() -> list[Document]:
     loader = JSONLoader(
     file_path='data/articles.json',
-    jq_schema='.response.results[].fields.body')
+    jq_schema='.response.results[].fields.headline')
 
     documents = loader.load()
     return documents
 
+
+'''
+Fonction split_data(...)
+
+Argument :
+documents --> list de "Document" 
+
+Retour : 
+Une liste de documents split et nettoyé des balises HTML 
+'''
+def split_data (documents):
+
+    #récupère les données au format JSON
+    #data = get_guardian_articles_data(query, pagesize=5)     
+        
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    documents = text_splitter.split_documents(documents)
+    for d in documents:
+        d.page_content=BeautifulSoup(d.page_content, "html.parser").get_text()
+
+    return documents
