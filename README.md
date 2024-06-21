5bcac699a8ba0a4e782bd2c6a36944c8cbca4faa
Alexandre Moreau <almoreaudev@gmail.com>
2024-06-21 15:18:46 +0200

    Feat : add chain to get openAi response
    Ajout d'une chaine ayant les paramètres pour accéder à chat-gpt.
    En utilisant chain.invoke(quesion) on obtient la réponse de chat-gpt sur la question donnée.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
+
 # ChatBot-python-news
- A simple chatbot python which give recent news.
+
+A simple chatbot python which give recent news.
+
+
+
+
+## Installation
+
+
+
+### 1. Python environnement
+
+First, install virtualenv for creating python virtual environnement
+```bash
+pip install virtualenv
+```
+Then, create the environnement
+
+```bash
+python3 -m venv env
+```
+
+Finally, activate the environnement
+```bash
+source env/bin/activate
+```
+
+### 2. Install the dependencies
+When the environnement is activate, you will see (.venv) at the start of your line command
+
+Exemple :
+```bash
+(.venv) $ _____
+```
+
+You need to install the dependencies with the file requirements.txt 
+```bash
+pip install -r requirements.txt
+```
+
+### 3. Prepare Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+Create a .env file with this :
+
+    .env
+    
+    `GUARDIAN_API_KEY`
+    `OPENAI_API_KEY`
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from src.data_collection import *
 from src.data_collection import *
 from src.data_preprocess import *
+from src.chatbot_prompt_template import *
 
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
 
 from pprint import pprint
 
-#récupère 20 articles de l'API de Guardian et le stocke dans un fichier (articles.json)
-data = get_guardian_articles_data("", pagesize=20)
+#La question qu'on pose au chat bot
+## à modifier pour tester
+question="Que c'est t'il passé récemment à propos de la France ?"
 
+
+
+
+#récupère 20 articles de l'API de Guardian et le stocke dans un fichier (articles.json)
+data = get_guardian_articles_data("", pagesize=100)
 
 documents = load_json_bodies_in_Document_list()
 documents = split_data(documents)
-pprint(documents)
 
 #Création de la base de donnée vectorielle "Chroma"
 #Nom de la base de données "articles"
 #Sauvegarde la database au chemin 'database/chroma/'
 #Utilise une fonction embedding par "défault", OpenAIEmbeddings()
-reviews_vector_db = Chroma(
+articles_chroma_db = Chroma(
     "articles",
     persist_directory='database/chroma/',
     embedding_function=OpenAIEmbeddings(),
@@ -27,17 +34,36 @@ reviews_vector_db = Chroma(
 
 
 #Ajout des documents à la base de donnée
-db = reviews_vector_db.from_documents(documents, OpenAIEmbeddings())
+articles_chroma_db = articles_chroma_db.from_documents(documents, OpenAIEmbeddings())
 
-
-
-question="What happen about space ?"
 
 #Compare la question avec les articles stockés pour trouver les 4 plus proches
-docs = db.similarity_search(question)
+'''docs = articles_chroma_db.similarity_search(question)
 
 for doc in docs:
     print(doc.page_content)
-    print (doc.metadata)
+    print (doc.metadata)'''
 
+#récupère la variable d'environnement OPENAI_API_KEY pour accéder à openai
+os.getenv('OPENAI_API_KEY')
+chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
 
+#récupération du template (voir chatbot_prompt_template.py)
+chatbot_prompt_template = chatbot_prompt_template()
+
+#chaine de données utilisée pour la requête à chat-gpt
+##"context" et "question" --> récupère k article dans la base de données ayant un rapport avec la question donnée
+##chatbot_prompt_template --> template pour la requête (voir chatbot_prompt_template.py)
+##chat_model --> le modèle de chat-gpt utilisé
+##StrOutputParser() --> envoie en réponse un string
+chatbot_chain = (
+    {"context": articles_chroma_db.as_retriever(k=4), "question": RunnablePassthrough()}
+    | chatbot_prompt_template
+    | chat_model
+    | StrOutputParser()
+)
+
+#envoie de la chaine de données à chatgpt afin de récupérer une réponse
+chatbot_response = chatbot_chain.invoke(question)
+
+print(chatbot_response)
